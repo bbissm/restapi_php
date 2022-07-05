f55d2330044032157223cb388f140a7999120700
bbissm <martin.bissiger@hotmail.com>
2022-07-05 20:55:51 +0200

    added Controller for Person to handle the API endpoints. added Person Gateway for implementing methods to return all records, return a specific person and add/update/delete a person. added class for PDO Database connection

---
+++ src/System/DatabaseConnector.php
@@ -0,0 +1,35 @@
+<?php
+namespace src\System;
+
+class DatabaseConnector {
+
+    private $dbConnection = null;
+
+    public function __construct()
+    {
+        $con_data = [
+            'host' => getenv('DB_HOST'),
+            'port' => getenv('DB_PORT'),
+            'db'   => getenv('DB_DATABASE'),
+            'user' => getenv('DB_USERNAME'),
+            'pass' => getenv('DB_PASSWORD'),
+        ];
+
+
+        try {
+
+            $this->dbConnection = new \PDO(
+                "mysql:host=".$con_data['host'].";port=".$con_data['port'].";charset=utf8mb4;dbname=".$con_data['db'].";",
+                $con_data['user']
+            );
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+
+    }
+
+    public function getConnection()
+    {
+        return $this->dbConnection;
+    }
+}
